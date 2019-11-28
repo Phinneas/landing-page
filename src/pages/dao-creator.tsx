be370245d06c12d103f11fa3a74102c6e3d7da9b
dOrgJelli <jelli@dorg.tech>
2019-11-27 23:20:38 -0500

    layout

--- src/pages/dao-creator.tsx
+++ src/pages/dao-creator.tsx
@@ -1,11 +1,12 @@
 import DAOcreator from "@dorgtech/daocreator-ui-v1";
+import Layout from "../components/Layout";
 import SEO from "../components/Seo";
 
 const DAOcreatorPage: React.FC = () => (
-  <>
+  <Layout>
     <SEO title="DAO Creator" />
     <DAOcreator />
-  </>
+  </Layout>
 );
 
 export default DAOcreatorPage;
