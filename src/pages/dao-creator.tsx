4767c63f3c143fc26ac5c1ed7dcda17ce9d0997d
dOrgJelli <jelli@dorg.tech>
2019-11-27 22:29:55 -0500

    not working...

---
+++ src/pages/dao-creator.tsx
@@ -0,0 +1,11 @@
+import DAOcreator from "@dorgtech/daocreator-ui-v1";
+import SEO from "../components/Seo";
+
+const DAOcreatorPage: React.FC = () => (
+  <>
+    <SEO title="DAO Creator" />
+    <DAOcreator />
+  </>
+);
+
+export default DAOcreatorPage;
