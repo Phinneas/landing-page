e5e9c06651917cf9334bab93579cdd68d0116895
Nick Place <nickjplace@gmail.com>
2020-01-31 20:22:03 -0700

    add window

--- src/util/windowShim.ts
+++ src/util/windowShim.ts
@@ -1,17 +1,17 @@
-const windowGlobal = typeof window !== "undefined" && window;
+// const windowGlobal = typeof window !== "undefined" && window;
 
 class WindowShim {
   open(url: string) {
-    (windowGlobal as Window).open(url);
+    // (windowGlobal as Window).open(url);
   }
   scrollToBottom() {
-    setTimeout(() => {
-      (windowGlobal as Window).scrollTo({
-        left: 0,
-        top: document.body.scrollHeight,
-        behavior: "smooth"
-      });
-    }, 200);
+    // setTimeout(() => {
+    //   (windowGlobal as Window).scrollTo({
+    //     left: 0,
+    //     top: document.body.scrollHeight,
+    //     behavior: "smooth"
+    //   });
+    // }, 200);
   }
 }
 
